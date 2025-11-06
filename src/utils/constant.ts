03b85b78885daf2f8a274dc8641ed5aa0f37575f
Aliez <8287771+aliez-ren@users.noreply.github.com>
2025-11-06 23:06:06 +0900

    Update constant.ts

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -9,7 +9,7 @@ export const usdc = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
 
 export const value = 0n
 
-export const gas = 400_000n
+export const gas = 200_000n
 
 export const client = createPublicClient({
   chain: ethereum,
